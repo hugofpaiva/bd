8d70f1b16caca478eee9dbdf4aa0f2afe4053085
hugofpaiva <hugofpaiva@ua.pt>
2020-05-28 23:54:51 +0100

    Continuação script python

--- Projeto/bd/Storage Procedures/changeProduct.sql
+++ Projeto/bd/Storage Procedures/changeProduct.sql
@@ -1,5 +1,6 @@
 GO
-CREATE PROCEDURE dbo.addProduct
+CREATE PROCEDURE dbo.changeProduct
+    @id INT,
     @preco INT,
     @familiaolfativa VARCHAR(30) = NULL,
     @categoria VARCHAR(30),
@@ -20,9 +21,9 @@ BEGIN
     BEGIN TRY
         IF EXISTS(SELECT email FROM Perfumaria.dbo.funcionario WHERE email=@emailFunc AND administrator>0)
             BEGIN
-                INSERT INTO Perfumaria.dbo.produto
-                (preco, familiaolfativa, categoria, nome, marca, linha, tamanho, descricao, imagem, stock, destinatario)
-                VALUES(@preco, @familiaolfativa, @categoria, @nome, @marca, @linha, @tamanho, @descricao, @imagem, @stock, @destinatario) 
+                UPDATE Perfumaria.dbo.produto
+                SET preco=@preco, familiaolfativa=@familiaolfativa, categoria=@categoria, nome=@nome, marca=@marca, linha=@linha, tamanho=@tamanho, descricao=@descricao, imagem=@imagem, stock=@stock, destinatario=@destinatario
+                WHERE id=@id
                 SET @responseMessage='Success'
             END
         ELSE
--- Projeto/insert.py
+++ Projeto/insert.py
@@ -6,18 +6,6 @@ loc = ("./dados.xlsx")
 # Abrir Workbook
 wb = xlrd.open_workbook(loc) 
 
-
-# Percorrer as sheets todas
-pointSheets = wb.sheet_names()
-counter=0
-for sheet in pointSheets:
-    print(sheet)
-    sheet = wb.sheet_by_index(counter)
-    for row in range(1,sheet.nrows):
-        for column in range(0, sheet.ncols):
-            print(sheet.cell_value(row, column))
-    counter+=1
-
 '''
 # Ligação à base de dados
 try:
@@ -30,10 +18,133 @@ try:
 except:
     print("Erro ao ligar à base de dados!")
     sys.exit()
-
-
-
 '''
+
+# Funções de Inserção
+
+
+def promocao(id, nome, desconto, datainicio, datafim):
+    print("Promocao")
+
+def produto_tem_promocao(produtoid, promocaoid):
+    print("Produtotem")
+
+def produto(id, preco, familiaolfativa, categoria, nome, marca, linha, tamanho, descricao, imagem, stock, destinatario):
+    print("Produto")
+
+def perfume(id):
+    print("Perfume")
+
+def cosmetica(id, tipo):
+    print("Cosmetica")
+
+def clientefavorita(clienteemail, produtoid):
+    print("Favorita")
+
+def cupao(id, datainicio, datafim, pontos_atribuidos):
+    print("CUPAO")
+
+def cliente_usa_cupao(cliente_email, cupao_id):
+    print("usacupa")
+
+def cliente(email, pontos, newsletter, pagamento):
+    print("clien")
+
+def utilizador(email, contribuinte, fname, lname, pw, sexo, dataNasc, foto, contacto_default_id):
+    print("utilizado")
+
+def funcionario(email, administrator, salario):
+    print("funcionario")
+
+def contacto(id, utilizador_email, telemovel, visibilidade, codigo_postal, pais, endereco, apartamento, localidade):
+    print("contacto")
+
+def compra_tem_produto(compranumero, produtoid, unidades):
+    print("compra_tem_produto")
+
+def compra(numero, contribuinte, datacompra, pagamento, clienteemail, pontosgastos, pontosacumulados):
+    print("Compra")
+
+def servico(id, tipo, preco):
+    print("servico")
+
+def funcionario_faz_servico(funcionario_email, servico_id, duracao_media):
+    print("funcionariofaz")
+
+def compra_online(numero, rating, observacao, rastreamento, presente, contactoid):
+    print("compra_online")
+
+def compra_presencial(numero, funcemail):
+    print("compra_presencial")
+
+def marcacao(id, cliente_email, servico_id, funcionario_email, dataMarc):
+    print("marcacao")
+
+
+def choose(sheet, values):
+    if sheet == "promocao":
+        promocao(values[0], values[1], values[2], values[3], values[4])
+    elif sheet == "produto_tem_promocao":
+        produto_tem_promocao(values[0], values[1])
+    elif sheet == "produto":
+        produto(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10], values[11])
+    elif sheet == "perfume":
+        perfume(values[0])
+    elif sheet == "cosmetica":
+        cosmetica(values[0], values[1])
+    elif sheet == "clientefavorita":
+        clientefavorita(values[0], values[1])
+    elif sheet == "cupao":
+        cupao(values[0], values[1], values[2], values[3])
+    elif sheet == "cliente_usa_cupao":
+        cliente_usa_cupao(values[0], values[1])
+    elif sheet == "cliente":
+        cliente(values[0], values[1], values[2], values[3])
+    elif sheet == "utilizador":
+        utilizador(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8])
+    elif sheet == "funcionario":
+        funcionario(values[0], values[1], values[2])
+    elif sheet == "contacto":
+        contacto(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8])
+    elif sheet == "compra_tem_produto":
+        compra_tem_produto(values[0], values[1], values[2])
+    elif sheet == "compra":
+        compra(values[0], values[1], values[2], values[3], values[4], values[5], values[6])
+    elif sheet == "servico":
+        servico(values[0], values[1], values[2])
+    elif sheet == "funcionario_faz_servico":
+        funcionario_faz_servico(values[0], values[1], values[2])
+    elif sheet == "compra_online":
+        compra_online(values[0], values[1], values[2], values[3], values[4], values[5])
+    elif sheet == "compra_presencial":
+        compra_presencial(values[0], values[1])
+    elif sheet == "marcacao":
+        marcacao(values[0], values[1], values[2], values[3], values[4])
+
+
+# Percorrer as sheets todas
+pointSheets = wb.sheet_names()
+counter=0
+for sheet_name in pointSheets:
+    sheet = wb.sheet_by_index(counter)
+    for row in range(1,sheet.nrows):
+        call = [sheet.cell_value(row, column) for column in range(0, sheet.ncols)]
+        choose(sheet_name, call)
+
+    counter+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''
 cursor.execute('SELECT * FROM TestDB.dbo.Person')
  
