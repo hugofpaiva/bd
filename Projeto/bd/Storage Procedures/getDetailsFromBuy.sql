7707a2d7363ad1889bf06af7596fb7735b0288ac
Pedro Bastos <hacker@MacBook-Pro.home>
2020-06-07 23:27:26 +0100

    getDetailsFromBuy SP

---
+++ Projeto/bd/Storage Procedures/getDetailsFromBuy.sql
@@ -0,0 +1,23 @@
+GO
+CREATE PROCEDURE perf.getDetailsFromBuy
+    @numero INT
+AS
+BEGIN
+        SET NOCOUNT ON
+            IF EXISTS(SELECT numero FROM Perfumaria.perf.compra_online WHERE numero=@numero)
+                BEGIN
+                    SELECT pagamento AS TipoPagamento, contribuinte, rating, rastreamento, presente, observacao, telemovel, codigo_postal AS codigoPostal, pais, endereco, apartamento, localidade 
+                    FROM ((Perfumaria.perf.compra JOIN Perfumaria.perf.compra_online ON compra.numero=compra_online.numero) JOIN Perfumaria.perf.contacto ON contactoid=id)
+                    WHERE compra.numero=@numero
+                END
+            ELSE IF EXISTS(SELECT numero FROM Perfumaria.perf.compra_presencial WHERE numero=@numero)
+                BEGIN
+                    SELECT pagamento AS TipoPagamento, compra.contribuinte, fname as Funcionario
+                    FROM (((Perfumaria.perf.compra JOIN Perfumaria.perf.compra_presencial ON compra.numero=compra_presencial.numero) JOIN Perfumaria.perf.funcionario ON funcemail=email) JOIN Perfumaria.perf.utilizador ON funcionario.email=utilizador.email)
+                    WHERE compra.numero=@numero
+                END
+END
+
+---------------
+--pronta a usar
+---------------
--- Projeto/bd/UDFs/clientBuyHistory.sql
+++ Projeto/bd/UDFs/clientBuyHistory.sql
@@ -1,8 +1,11 @@
 CREATE FUNCTION perf.clientBuyHistory (@email VARCHAR(255)) RETURNS TABLE 
 AS
-    RETURN (SELECT datacompra, SUM(unidades * preco) as total, compranumero
+    RETURN (SELECT datacompra as [Data], SUM(unidades * preco) as Total, compranumero
                     FROM ((Perfumaria.perf.compra_tem_produto JOIN Perfumaria.perf.compra ON compra_tem_produto.compranumero=compra.numero) JOIN Perfumaria.perf.produto ON produtoid=id)
                     WHERE (clienteemail=@email)
                     GROUP BY datacompra, clienteemail, compranumero
                     ORDER BY datacompra DESC OFFSET 0 ROWS)
 GO 
+-----------------
+-- pronta a usar
+-----------------
