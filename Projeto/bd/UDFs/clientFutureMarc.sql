aa217ea0bda450830fe06deec2bac77701997442
hugofpaiva <hugofpaiva@ua.pt>
2020-06-07 22:43:08 +0100

    changes

--- Projeto/bd/UDFs/clientFutureMarc.sql
+++ Projeto/bd/UDFs/clientFutureMarc.sql
@@ -2,8 +2,8 @@
 
 CREATE FUNCTION perf.clientFutureMarc (@email VARCHAR(255)) RETURNS TABLE 
 AS
-    RETURN (SELECT tipo, preço, fname, lname, photo, dataMarc
+    RETURN (SELECT tipo, preco, fname, lname, foto, dataMarc
                     FROM ((Perfumaria.perf.marcacao JOIN Perfumaria.perf.servico ON servico_id=id) JOIN Perfumaria.perf.utilizador ON cliente_email = email)
                     WHERE (cliente_email=@email AND DATEDIFF(mi, GETDATE(), dataMarc) > 0) 
-                    ORDER BY dataMarc ASC)             
+                    ORDER BY dataMarc ASC)              
 GO
