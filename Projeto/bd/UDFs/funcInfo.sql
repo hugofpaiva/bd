9082af86e4f541bf074a443b618286469811301d
Pedro Bastos <hacker@MacBook-Pro.local>
2020-06-09 00:22:32 +0100

    udfs

--- Projeto/bd/UDFs/funcInfo.sql
+++ Projeto/bd/UDFs/funcInfo.sql
@@ -1,12 +1,12 @@
-DROP PROCEDURE perf.getFuncInfo;
+DROP FUNCTION perf.getFuncInfo;
 GO
 
-CREATE FUNCTION perf.getFuncInfo (@email VARCHAR(255)) RETURNS Table 
+CREATE FUNCTION perf.getFuncInfo (@emailFunc VARCHAR(255)) RETURNS Table 
 AS
-    RETURN (SELECT * 
+    RETURN (SELECT Perfumaria.perf.utilizador.*, Perfumaria.perf.funcionario.salario, Perfumaria.perf.funcionario.administrator
                     FROM Perfumaria.perf.utilizador
                     JOIN Perfumaria.perf.funcionario ON utilizador.email = funcionario.email
-                    WHERE @email = utilizador.email) 
-GO           
+                    WHERE utilizador.email = @emailFunc) 
+GO         
                 
 --SELECT * FROM perf.AveragePricebyItems (15.00)
