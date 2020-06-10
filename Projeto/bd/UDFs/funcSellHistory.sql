8342652e5125ab350c89557a45c16e05e2cfb543
Pedro Bastos <hacker@MacBook-Pro.home>
2020-06-10 10:55:32 +0100

    update udf

--- Projeto/bd/UDFs/funcSellHistory.sql
+++ Projeto/bd/UDFs/funcSellHistory.sql
@@ -1,7 +1,10 @@
+DROP FUNCTION perf.funcSellHistory;
+GO
+
 CREATE FUNCTION perf.funcSellHistory (@email VARCHAR(255)) RETURNS TABLE 
 AS
     RETURN (SELECT datacompra, clienteemail, SUM(unidades * preco) as total, compranumero
-                    FROM ((Perfumaria.perf.compra_tem_produto JOIN Perfumaria.perf.compra ON compra_presencial.numero=compra.numero)JOIN Perfumaria.perf.produto ON produtoid=id)
+                    FROM (((Perfumaria.perf.compra_tem_produto JOIN Perfumaria.perf.compra ON compranumero=numero)JOIN Perfumaria.perf.produto ON produtoid=id) JOIN Perfumaria.perf.compra_presencial ON compra_presencial.numero=compra.numero)
                     WHERE (funcemail=@email)
                     GROUP BY datacompra, clienteemail, compranumero
                     ORDER BY datacompra DESC)
