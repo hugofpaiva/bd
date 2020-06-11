46fd530c2359d63c38d4b8e390795e6e9e4fab1f
Pedro Bastos <hacker@MacBook-Pro.home>
2020-06-11 17:48:27 +0100

    updates

---
+++ Projeto/bd/UDFs/getAllFuncs.sql
@@ -0,0 +1,18 @@
+DROP function perf.getAllFuncs;
+GO
+
+CREATE function perf.getAllFuncs ( @emailFunc VARCHAR(255)) returns @table table (email VARCHAR(255) NOT NULL, contribuinte CHAR(9) NOT NULL, fname VARCHAR(20) NOT NULL,
+                                                                                lname VARCHAR(20) NOT NULL, sexo BIT NOT NULL, dataNasc DATE NOT NULL,
+                                                                                foto VARCHAR(100) NOT NULL, contacto_default_id INT,  
+                                                                                administrator TINYINT NOT NULL, salario INT NOT NULL, deleted BIT NOT NULL DEFAULT 0)                                            
+AS
+BEGIN 
+            DECLARE @email AS VARCHAR(255), @contribuinte AS CHAR(9), @fname AS VARCHAR(20),
+                    @lname AS VARCHAR(20), @sexo AS BIT, @dataNasc AS DATE,
+                    @foto AS VARCHAR(100), @contacto_default_id AS INT, @deleted AS BIT, 
+                    @administrator AS TINYINT, @salario AS INT;
+            IF EXISTS(SELECT email FROM Perfumaria.perf.funcionario WHERE email=@emailFunc AND administrator=2)
+                INSERT @table SELECT utilizador.email, contribuinte, fname, lname, sexo, dataNasc, foto, contacto_default_id, administrator, salario, deleted
+                            FROM Perfumaria.perf.utilizador JOIN Perfumaria.perf.funcionario ON utilizador.email=funcionario.email
+            RETURN;
+        END;
