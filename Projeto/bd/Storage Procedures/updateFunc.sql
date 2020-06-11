72917fd30d1d35772968dba80d6c65663d98682a
Pedro Bastos <hacker@MacBook-Pro.home>
2020-06-11 20:21:51 +0100

    updates

--- Projeto/bd/Storage Procedures/updateFunc.sql
+++ Projeto/bd/Storage Procedures/updateFunc.sql
@@ -4,68 +4,45 @@ GO
 GO
 CREATE PROCEDURE perf.updateFunc
 	@email VARCHAR(255), 
-    @password VARCHAR(25), 
-    @fname VARCHAR(20),
-    @lname VARCHAR(20),
-    @admin TINYINT,
-    @salario INT,
-    @emailOP VARCHAR(255) = NULL, -- Só introduzido na parte de gerir funcionários de admin = 2
+    @admin TINYINT = null,
+    @salario INT = null,
+    @emailOP VARCHAR(255),
     @responseMsg nvarchar(250) output
 AS
 BEGIN
 	SET NOCOUNT ON
 
 	BEGIN TRY
-
-		IF @fname <> 'None'
-		BEGIN
-			UPDATE Perfumaria.perf.utilizador
-			SET fname = @fname
-			WHERE email = @email
-		END
-
-		IF @lname <> 'None'
-		BEGIN
-			UPDATE Perfumaria.perf.utilizador
-			SET lname = @lname
-			WHERE  email = @email
-		END
-
-		IF @password <> 'None'
-		BEGIN
-			UPDATE Perfumaria.perf.utilizador
-			SET pw = hashbytes('SHA2_512', @password)
-			WHERE  email = @email
-		END
-
-        IF @admin <> 'None'
+        IF @admin IS NOT NULL
 		BEGIN
             IF EXISTS (SELECT administrator FROM Perfumaria.perf.funcionario WHERE email = @email AND administrator = 2)
             BEGIN
                 UPDATE Perfumaria.perf.funcionario
                 SET administrator = @admin
                 WHERE  email = @emailOP
+				SET @responseMsg='Success'
             END
             ELSE
                 RAISERROR ('Insuficient Permissions.', 14, 1);
 		END
 
-        IF @salario <> 'None'
+        IF @salario IS NOT NULL
 		BEGIN
             IF EXISTS (SELECT administrator FROM Perfumaria.perf.funcionario WHERE email = @email AND administrator = 2)
             BEGIN
                 UPDATE Perfumaria.perf.funcionario
                 SET salario = @salario
                 WHERE  email = @emailOP
+				SET @responseMsg='Success'
             END
             ELSE
                 RAISERROR ('Insuficient Permissions.', 14, 1);
 		END
 
-		SET @responseMsg='Success'
+		
 	END TRY
 	BEGIN CATCH
-		SET @responseMsg=error_message()
+		SET @responseMsg='Failed'
 	END CATCH
 END
 go
